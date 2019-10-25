57bdfdc80a7d04fda53e64b722ac3ec76b02fd36
Jock Reed <jock.da.rock@gmail.com>
2019-10-24 21:39:13 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 # k3s install and developer association
-sudo wget -O /home/developer/k3s https://github.com/rancher/k3s/releases/download/v0.10.0/k3s
+# sudo wget -O /home/developer/k3s https://github.com/rancher/k3s/releases/download/v0.10.0/k3s
 sudo docker pull ciscodevnet/vs-code-iox-client:latest
-sudo chmod +x /home/developer/k3s
-sudo chown developer /home/developer/k3s
-sudo chgrp developer /home/developer/k3s
+sudo docker run --privileged --restart always -d -p 8443:8443 -p 6443:6443 ciscodevnet/vs-code-iox-client:latest
+#sudo chmod +x /home/developer/k3s
+#sudo chown developer /home/developer/k3s
+#sudo chgrp developer /home/developer/k3s
 sudo usermod -a -G docker developer
-sudo mkdir /var/lib/rancher
-sudo chown developer /var/lib/rancher
-sudo chgrp developer /var/lib/rancher
+#sudo mkdir /var/lib/rancher
+#sudo chown developer /var/lib/rancher
+#sudo chgrp developer /var/lib/rancher
 
 # Make sure BashRC profile exists
 FILE=/home/developer/.bashrc
